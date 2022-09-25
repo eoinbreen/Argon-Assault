eb18837f48034f27ab0b2fb4b54bbe8820fe6fed
eoinbreen <e.breen185@gmail.com>
2022-09-25 19:02:06 +0100

    Clamped X and Y Positions to Camera View

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,6 +7,9 @@ public class PlayerControls : MonoBehaviour
 {
     [SerializeField] InputAction movement;
     [SerializeField] float movementSpeed = 20f;
+
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 7f;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,11 +28,17 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //Y Clamp (-8f, 11f) X Clamp (-16f, 16f)
         float xMovement = movement.ReadValue<Vector2>().x * Time.deltaTime;
         float yMovement = movement.ReadValue<Vector2>().y * Time.deltaTime;
 
-        float newXPos = transform.localPosition.x + (xMovement * movementSpeed);
-        float newYPos = transform.localPosition.y + (yMovement * movementSpeed);
-        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+        float rawXPos = transform.localPosition.x + (xMovement * movementSpeed);
+        float rawYPos = transform.localPosition.y + (yMovement * movementSpeed);
+
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }

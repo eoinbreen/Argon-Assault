a68b9eb21ba15dac56df364fde26587ed7064ccf
eoinbreen <e.breen185@gmail.com>
2022-10-03 22:13:08 +0100

    Play Explosion on Collision

--- Assets/Scripts/CollisionHandeler.cs
+++ Assets/Scripts/CollisionHandeler.cs
@@ -6,16 +6,25 @@ using UnityEngine.SceneManagement;
 public class CollisionHandeler : MonoBehaviour
 {
     int currentLevel;
-   
+    [SerializeField] ParticleSystem explosion;
 
     private void OnTriggerEnter(Collider other)
     {
+        Crash();
+    }
+
+    void Crash()
+    {
+        explosion.Play();
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Renderer>().enabled = false;
         GetComponent<PlayerControls>().enabled = false;// disable PlayerControls Script
         Invoke("ReloadLevel", 1f);//1 second delay on reloading Level to play audio and particles
     }
 
     void ReloadLevel()
     {
+       
         currentLevel = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentLevel);
     }

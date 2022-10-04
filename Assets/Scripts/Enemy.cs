73568d81a92f8c8b0fba011cfc25ed39d37cff58
eoinbreen <e.breen185@gmail.com>
2022-10-04 17:47:25 +0100

    Added Scoreboard

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,7 +5,27 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] ParticleSystem explosion;
+
+    [SerializeField] int pointValue = 1;
+    
+    ScoreBoard scoreBoard;
+
+    private void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>(); // Keeps same scoreboard throughout game
+    }
     private void OnParticleCollision(GameObject other)
+    {
+        ProcessScore();
+        KillEnemy();
+    }
+
+    private void ProcessScore()
+    {
+        scoreBoard.IncreaseScore(pointValue);
+    }
+
+    private void KillEnemy()
     {
         Instantiate(explosion, transform.position, Quaternion.identity);
         Destroy(gameObject);
